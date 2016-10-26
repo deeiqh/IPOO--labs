fbd314a26a89a4c36e5e30d4694b3a3bf2d726d9
k-st <kast46.15@gmail.com>
2016-10-25 20:14:04 -0500

    Update curvas.h

--- figuras_geometricas/curvas.h
+++ figuras_geometricas/curvas.h
@@ -9,7 +9,7 @@ public:
 
     Punto *puntos;
     Curvas(const char *nombre, const int cantidad_puntos = 3): Formas(nombre, cantidad_puntos), puntos(new Punto[cantidad_puntos]){}
-
+    // cantidad_puntos es 3 porque se asumira que el primer elemento es el punto, el segundo el eje mayor y el tercero el eje menor
     TPunto area() const override;
     TPunto perimetro() const override;
     ~Curvas(){delete[] puntos;}
